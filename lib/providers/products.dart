9c1f6e203d29949559e6e139c37ca320fea76a3d
yoshinoseiji <sij.ysn25@gmail.com>
2022-02-17 00:58:18 +0900

    Introducing HTTP request

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -66,16 +69,30 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct(Product product) {
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-    _items.insert(0, newProduct); // at the first of list
-    notifyListeners();
+  void addProduct(Product product) async {
+    const url =
+        'https://shop-app-f24a9-default-rtdb.firebaseio.com/products.json';
+    var response = await http.post(url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'imageUrl': product.imageUrl,
+          'price': product.price,
+          'isFavorite': product.isFavorite,
+        }));
+    if (response != null) {
+      print(json.decode(response.body));
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // at the first of list
+      notifyListeners();
+    }
   }
 
   void updateProduct(String id, Product newProduct) {
